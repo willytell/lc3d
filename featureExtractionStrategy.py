5d5ed2eef6ac1bd72580947390260b433fa5d5ac
willytell <guille.torres@gmail.com>
2018-11-23 21:37:01 +0100

    save to csv

--- featureExtractionStrategy.py
+++ featureExtractionStrategy.py
@@ -1,102 +1,104 @@
 from abc import ABC, abstractmethod
 import SimpleITK as sitk
 import numpy as np
+import pandas as pd
+import os
 import six
 import radiomics
 from radiomics import featureextractor  # This module is used for interaction with pyradiomics
 
 
-def rolling_window(array, window=(0,), asteps=None, wsteps=None, axes=None, toend=True):
-
-    array = np.asarray(array)
-    orig_shape = np.asarray(array.shape)
-    window = np.atleast_1d(window).astype(int)  # maybe crude to cast to int...
-
-    if axes is not None:
-        axes = np.atleast_1d(axes)
-        w = np.zeros(array.ndim, dtype=int)
-        for axis, size in zip(axes, window):
-            w[axis] = size
-        window = w
-
-    # Check if window is legal:
-    if window.ndim > 1:
-        raise ValueError("`window` must be one-dimensional.")
-    if np.any(window < 0):
-        raise ValueError("All elements of `window` must be larger then 1.")
-    if len(array.shape) < len(window):
-        raise ValueError("`window` length must be less or equal `array` dimension.")
-
-    _asteps = np.ones_like(orig_shape)
-    if asteps is not None:
-        asteps = np.atleast_1d(asteps)
-        if asteps.ndim != 1:
-            raise ValueError("`asteps` must be either a scalar or one dimensional.")
-        if len(asteps) > array.ndim:
-            raise ValueError("`asteps` cannot be longer then the `array` dimension.")
-        # does not enforce alignment, so that steps can be same as window too.
-        _asteps[-len(asteps):] = asteps
-
-        if np.any(asteps < 1):
-            raise ValueError("All elements of `asteps` must be larger then 1.")
-    asteps = _asteps
-
-    _wsteps = np.ones_like(window)
-    if wsteps is not None:
-        wsteps = np.atleast_1d(wsteps)
-        if wsteps.shape != window.shape:
-            raise ValueError("`wsteps` must have the same shape as `window`.")
-        if np.any(wsteps < 0):
-            raise ValueError("All elements of `wsteps` must be larger then 0.")
-
-        _wsteps[:] = wsteps
-        _wsteps[window == 0] = 1  # make sure that steps are 1 for non-existing dims.
-    wsteps = _wsteps
-
-    # Check that the window would not be larger then the original:
-    if np.any(orig_shape[-len(window):] < window * wsteps):
-        raise ValueError("`window` * `wsteps` larger then `array` in at least one dimension.")
-
-    new_shape = orig_shape  # just renaming...
-
-    # For calculating the new shape 0s must act like 1s:
-    _window = window.copy()
-    _window[_window == 0] = 1
-
-    new_shape[-len(window):] += wsteps - _window * wsteps
-    new_shape = (new_shape + asteps - 1) // asteps
-    # make sure the new_shape is at least 1 in any "old" dimension (ie. steps
-    # is (too) large, but we do not care.
-    new_shape[new_shape < 1] = 1
-    shape = new_shape
-
-    strides = np.asarray(array.strides)
-    strides *= asteps
-    new_strides = array.strides[-len(window):] * wsteps
-
-    # The full new shape and strides:
-    if toend:
-        new_shape = np.concatenate((shape, window))
-        new_strides = np.concatenate((strides, new_strides))
-    else:
-        _ = np.zeros_like(shape)
-        _[-len(window):] = window
-        _window = _.copy()
-        _[-len(window):] = new_strides
-        _new_strides = _
-
-        new_shape = np.zeros(len(shape) * 2, dtype=int)
-        new_strides = np.zeros(len(shape) * 2, dtype=int)
-
-        new_shape[::2] = shape
-        new_strides[::2] = strides
-        new_shape[1::2] = _window
-        new_strides[1::2] = _new_strides
-
-    new_strides = new_strides[new_shape != 0]
-    new_shape = new_shape[new_shape != 0]
-
-    return np.lib.stride_tricks.as_strided(array, shape=new_shape, strides=new_strides)
+# def rolling_window(array, window=(0,), asteps=None, wsteps=None, axes=None, toend=True):
+#
+#     array = np.asarray(array)
+#     orig_shape = np.asarray(array.shape)
+#     window = np.atleast_1d(window).astype(int)  # maybe crude to cast to int...
+#
+#     if axes is not None:
+#         axes = np.atleast_1d(axes)
+#         w = np.zeros(array.ndim, dtype=int)
+#         for axis, size in zip(axes, window):
+#             w[axis] = size
+#         window = w
+#
+#     # Check if window is legal:
+#     if window.ndim > 1:
+#         raise ValueError("`window` must be one-dimensional.")
+#     if np.any(window < 0):
+#         raise ValueError("All elements of `window` must be larger then 1.")
+#     if len(array.shape) < len(window):
+#         raise ValueError("`window` length must be less or equal `array` dimension.")
+#
+#     _asteps = np.ones_like(orig_shape)
+#     if asteps is not None:
+#         asteps = np.atleast_1d(asteps)
+#         if asteps.ndim != 1:
+#             raise ValueError("`asteps` must be either a scalar or one dimensional.")
+#         if len(asteps) > array.ndim:
+#             raise ValueError("`asteps` cannot be longer then the `array` dimension.")
+#         # does not enforce alignment, so that steps can be same as window too.
+#         _asteps[-len(asteps):] = asteps
+#
+#         if np.any(asteps < 1):
+#             raise ValueError("All elements of `asteps` must be larger then 1.")
+#     asteps = _asteps
+#
+#     _wsteps = np.ones_like(window)
+#     if wsteps is not None:
+#         wsteps = np.atleast_1d(wsteps)
+#         if wsteps.shape != window.shape:
+#             raise ValueError("`wsteps` must have the same shape as `window`.")
+#         if np.any(wsteps < 0):
+#             raise ValueError("All elements of `wsteps` must be larger then 0.")
+#
+#         _wsteps[:] = wsteps
+#         _wsteps[window == 0] = 1  # make sure that steps are 1 for non-existing dims.
+#     wsteps = _wsteps
+#
+#     # Check that the window would not be larger then the original:
+#     if np.any(orig_shape[-len(window):] < window * wsteps):
+#         raise ValueError("`window` * `wsteps` larger then `array` in at least one dimension.")
+#
+#     new_shape = orig_shape  # just renaming...
+#
+#     # For calculating the new shape 0s must act like 1s:
+#     _window = window.copy()
+#     _window[_window == 0] = 1
+#
+#     new_shape[-len(window):] += wsteps - _window * wsteps
+#     new_shape = (new_shape + asteps - 1) // asteps
+#     # make sure the new_shape is at least 1 in any "old" dimension (ie. steps
+#     # is (too) large, but we do not care.
+#     new_shape[new_shape < 1] = 1
+#     shape = new_shape
+#
+#     strides = np.asarray(array.strides)
+#     strides *= asteps
+#     new_strides = array.strides[-len(window):] * wsteps
+#
+#     # The full new shape and strides:
+#     if toend:
+#         new_shape = np.concatenate((shape, window))
+#         new_strides = np.concatenate((strides, new_strides))
+#     else:
+#         _ = np.zeros_like(shape)
+#         _[-len(window):] = window
+#         _window = _.copy()
+#         _[-len(window):] = new_strides
+#         _new_strides = _
+#
+#         new_shape = np.zeros(len(shape) * 2, dtype=int)
+#         new_strides = np.zeros(len(shape) * 2, dtype=int)
+#
+#         new_shape[::2] = shape
+#         new_strides[::2] = strides
+#         new_shape[1::2] = _window
+#         new_strides[1::2] = _new_strides
+#
+#     new_strides = new_strides[new_shape != 0]
+#     new_shape = new_shape[new_shape != 0]
+#
+#     return np.lib.stride_tricks.as_strided(array, shape=new_shape, strides=new_strides)
 
 # Abstract class
 class FeatureExtractionStrategy(ABC):
@@ -110,7 +112,10 @@ class FeatureExtractionStrategy(ABC):
 
 
 class MyRadiomic(FeatureExtractionStrategy):
-    def __init__(self, name):
+    def __init__(self, name, csvFilePath, sep, encoding):
+        self.csvFilePath = csvFilePath
+        self.sep = sep
+        self.enconding = encoding
         self.maskITK = None
         self.extractor = None
         super().__init__(name)
@@ -129,6 +134,20 @@ class MyRadiomic(FeatureExtractionStrategy):
         print('Enabled filters:\n\t', self.extractor._enabledImagetypes)
         print('Enabled features:\n\t', self.extractor._enabledFeatures)
 
+    def appendDFToCSV_void(self, df, csvFilePath, sep=",", encoding='utf-8'):
+        if not os.path.isfile(csvFilePath):
+            df.to_csv(csvFilePath, mode='a', index=False, sep=sep, encoding=encoding)
+        elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
+            raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) +
+                            " columns. CSV file has " +
+                            str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns))
+                            + " columns.")
+        elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
+            raise Exception("Columns and column order of dataframe and csv file do not match!!")
+        else:
+            df.to_csv(csvFilePath, mode='a', index=False, sep=sep, encoding=encoding, header=False)
+
+
 
     def featureExtraction(self, array):
         print("Using Radiomic to extract features...")
@@ -138,24 +157,31 @@ class MyRadiomic(FeatureExtractionStrategy):
 
         max_i, max_j, max_k = array.shape[:3]
 
+        mydict = []
         for i in range(max_i):
             for j in range(max_j):
                 for k in range(max_k):
                     volume = array[i, j, k]
                     imageITK = sitk.GetImageFromArray(volume)
                     featureVector = self.extractor.execute(imageITK, self.maskITK)   # allways is used the same mask
-                    print("-----------------------------------------")
-                    print('Result type:', type(featureVector))  # result is returned in a Python ordered dictionary)
-                    print('')
-                    print('Calculated features')
+                    #print("-----------------------------------------")
+                    #print("Result type: {} and length: {}".format(type(featureVector), len(featureVector)))  # result is returned in a Python ordered dictionary)
+                    #print('')
+                    #print('Calculated features')
 
                     # Show output
+                    new_row = {}
                     for featureName in featureVector.keys():
                         #print('Computed %s: %s' % (featureName, featureVector[featureName]))
-                        print(featureVector[featureName])
+                        #print(featureVector[featureName])
+                        if ('firstorder' in featureName) or ('glszm' in featureName):
+                            new_row.update({featureName: featureVector[featureName]})
 
-                    #for key, value in six.iteritems(featureVector):
-                    #    print('\t', key, ':', value)
+                    #new_row = sorted(new_row)
+                    mydict.append(new_row)
+
+        df = pd.DataFrame.from_dict(mydict)
+        self.appendDFToCSV_void(df=df, csvFilePath=self.csvFilePath, sep=self.sep, encoding=self.enconding)
 
 
 
